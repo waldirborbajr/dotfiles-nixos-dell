7e933a126eb8149ca3b6818bcdb3139c3147c7b4
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-14 22:44:20 -0300

    Update configuration.nixwip

--- configuration.nix
+++ configuration.nix
@@ -6,37 +6,36 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+    [ ./hardware-configuration.nix ];
 
-  # Bootloader.
+  ############################################
+  # Bootloader
+  ############################################
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
+  ############################################
+  # Networking
+  ############################################
+  networking.hostName = "nixos";
   networking.networkmanager.enable = true;
 
+  ############################################
+  # Nix Garbage Collection (optimized)
+  ############################################
   nix.gc = {
     automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 7d";
+    dates = "daily";
+    options = "--delete-older-than 2d";
   };
 
-  # Set your time zone.
+  ############################################
+  # Locale / Time
+  ############################################
   time.timeZone = "America/Sao_Paulo";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "pt_BR.UTF-8";
     LC_IDENTIFICATION = "pt_BR.UTF-8";
@@ -49,26 +48,25 @@
     LC_TIME = "pt_BR.UTF-8";
   };
 
-  # Enable the X11 windowing system.
+  ############################################
+  # X11 + GNOME
+  ############################################
   services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
-  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "br";
     variant = "";
   };
 
-  # Configure console keymap
   console.keyMap = "br-abnt2";
 
-  # Enable CUPS to print documents.
+  ############################################
+  # Printing / Audio
+  ############################################
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -76,46 +74,37 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  ############################################
+  # User
+  ############################################
   users.users.borba = {
     isNormalUser = true;
     description = "BORBA JR W";
     extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.zsh;
     packages = with pkgs; [
-    #  thunderbird
       neovim
     ];
-    shell = pkgs.zsh;
   };
 
-
-  # Enable automatic login for the user.
   services.xserver.displayManager.autoLogin.enable = true;
   services.xserver.displayManager.autoLogin.user = "borba";
 
-  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
 
-  # Install firefox.
+  ############################################
+  # Programs
+  ############################################
   programs = {
     firefox.enable = true;
     zsh.enable = true;
   };
 
   ############################################
-  # Make Alacritty the default terminal
+  # Default Terminal
   ############################################
   environment.variables = {
     TERMINAL = "alacritty";
@@ -125,13 +114,12 @@
     "application/x-terminal-emulator" = "alacritty.desktop";
   };
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  ############################################
+  # System Packages
+  ############################################
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     eza
     btop
@@ -146,20 +134,21 @@
     rofi
     keepassxc
     waybar
-    pkgs.networkmanagerapplet
+    networkmanagerapplet
     xclip
     coreutils
     tmux
-    # --- Go ---
+
+    # Go
     go
     gopls
 
-    # --- Rust ---
+    # Rust
     rustup
     cargo
     rust-analyzer
 
-    # --- Nix ---
+    # Nix
     nixd
     nil
     statix
@@ -167,54 +156,40 @@
     nixfmt-rfc-style
   ];
 
+  ############################################
+  # Fonts
+  ############################################
   fonts = {
     packages = with pkgs; [
       nerd-fonts.terminess-ttf
       nerd-fonts.blex-mono
+      nerd-fonts.jetbrains-mono
       ibm-plex
       openmoji-color
     ];
-    fontconfig = {
-        defaultFonts = {
-          monospace = [ "JetBrainsMono Nerd Font" ];
-          sansSerif = [ "IBM Plex Sans" ];
-          serif = [ "IBM Plex Serif" ];
-         # monospace = [ "Terminess Nerd Font" ];
-          emoji = [ "OpenMoji Color" ];
-        };
+
+    fontconfig.defaultFonts = {
+      monospace = [ "JetBrainsMono Nerd Font" ];
+      sansSerif = [ "IBM Plex Sans" ];
+      serif = [ "IBM Plex Serif" ];
+      emoji = [ "OpenMoji Color" ];
     };
+
     enableDefaultFonts = true;
   };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
+  ############################################
+  # Services
+  ############################################
   services.openssh.enable = true;
-
-  # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [ 22 ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
-
+  ############################################
+  # Nix Features
+  ############################################
   nix.extraOptions = ''
     experimental-features = nix-command flakes
   '';
 
+  system.stateVersion = "25.11";
 }
