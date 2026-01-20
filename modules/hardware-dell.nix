4f4187c56eac09b891a77f587bc081e36006529a
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-19 23:32:48 -0300

    Update hardware-dell.nix

--- modules/hardware-dell.nix
+++ modules/hardware-dell.nix
@@ -2,11 +2,19 @@
 
 {
   ############################################
-  # Wi-Fi e Bluetooth
+  # Wi-Fi e Bluetooth (Dell Inspiron)
   ############################################
   networking.networkmanager.enable = true;
 
   hardware.enableRedistributableFirmware = true;
+
+  # Pacotes de firmware necessários
+  environment.systemPackages = with pkgs; [
+    firmware.b43         # Broadcom Wi-Fi
+    firmware.b43-open    # Versão open do firmware
+    firmware.intel-ucode # Microcode Intel
+    firmware.broadcom-wl # Broadcom STA (opcional, mais recente)
+  ];
 
   hardware.bluetooth = {
     enable = true;
@@ -36,21 +44,23 @@
     layout = "br";
     variant = "";
   };
-
   console.keyMap = "br-abnt2";
 
   ############################################
-  # Bootloader (GRUB + desativa os-prober)
+  # Bootloader (GRUB)
   ############################################
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = false;
-  boot.loader.grub.extraEntries = '' '';
 
   ############################################
   # Kernel Modules
   ############################################
-  boot.initrd.availableKernelModules = [ "ehci_pci" "ahci" "ums_realtek" "usb_storage" "sd_mod" "sr_mod" ];
+  boot.initrd.availableKernelModules = [
+    "ehci_pci" "ahci" "ums_realtek" "usb_storage" "sd_mod" "sr_mod"
+    "b43"       # Broadcom Wi-Fi
+    "btusb"     # Bluetooth USB
+  ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 }
