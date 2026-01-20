5f4beedd31d3f59a4e249481e55e84f9183e692c
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-20 00:16:36 -0300

    GROK change

--- configuration.nix
+++ configuration.nix
@@ -1,5 +1,4 @@
 { config, pkgs, ... }:
-
 {
   ############################################
   # Imports
@@ -10,24 +9,20 @@
     ##########################################
     ./hardware-configuration-dell.nix
     ./modules/hardware-dell.nix
-
     ##########################################
     # Hardware profile - MacBook (EFI)
     # Enable ONLY when running on MacBook
     ##########################################
     # ./modules/hardware-macbook.nix
     # ./modules/hardware-macbook-efi.nix
-
     ##########################################
     # Radio / Serial devices (CHIRP)
     ##########################################
     ./modules/hardware-radio-chirp.nix
-
     ##########################################
     # Kernel / performance tuning
     ##########################################
     ./modules/kernel-tuning.nix
-
     ##########################################
     # Desktop environment (choose ONE)
     ##########################################
@@ -36,14 +31,12 @@
     # ./modules/desktop-hyprland.nix
     # ./modules/desktop-lxqt.nix
     # ./modules/desktop-cinnamon.nix
-
     ##########################################
     # Base system
     ##########################################
     ./modules/fonts.nix
     ./modules/system-programs.nix
     ./modules/system-packages.nix
-
     ##########################################
     # Containers / Orchestration
     # Docker + K3s enabled by default
@@ -51,24 +44,20 @@
     ./modules/containers/docker.nix
     ./modules/containers/k3s.nix
     # ./modules/containers/podman.nix
-
     ##########################################
     # Optional virtualization tuning
     ##########################################
     # ./modules/virtualisation-bridge.nix
     # ./modules/virt-wayland-tuning.nix
-
     ##########################################
     # Maintenance / Garbage collection
     ##########################################
     ./modules/maintenance.nix
     ./modules/maintenance-hm.nix
-
     ##########################################
     # Users
     ##########################################
     ./modules/user-borba.nix
-
     ##########################################
     # Nix (unstable overlay)
     ##########################################
@@ -84,25 +73,23 @@
   # Locale / Time
   ############################################
   time.timeZone = "America/Sao_Paulo";
-
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
-    LC_ADDRESS        = "pt_BR.UTF-8";
+    LC_ADDRESS = "pt_BR.UTF-8";
     LC_IDENTIFICATION = "pt_BR.UTF-8";
-    LC_MEASUREMENT    = "pt_BR.UTF-8";
-    LC_MONETARY       = "pt_BR.UTF-8";
-    LC_NAME           = "pt_BR.UTF-8";
-    LC_NUMERIC        = "pt_BR.UTF-8";
-    LC_PAPER          = "pt_BR.UTF-8";
-    LC_TELEPHONE      = "pt_BR.UTF-8";
-    LC_TIME           = "pt_BR.UTF-8";
+    LC_MEASUREMENT = "pt_BR.UTF-8";
+    LC_MONETARY = "pt_BR.UTF-8";
+    LC_NAME = "pt_BR.UTF-8";
+    LC_NUMERIC = "pt_BR.UTF-8";
+    LC_PAPER = "pt_BR.UTF-8";
+    LC_TELEPHONE = "pt_BR.UTF-8";
+    LC_TIME = "pt_BR.UTF-8";
   };
 
   ############################################
   # Nix configuration
   ############################################
   nixpkgs.config.allowUnfree = true;
-
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
@@ -113,6 +100,45 @@
   ############################################
   services.openssh.enable = true;
   networking.firewall.allowedTCPPorts = [ 22 ];
+
+  ############################################
+  # Wi-Fi e Bluetooth (Broadcom BCM4312 fixes)
+  ############################################
+  networking.networkmanager.enable = true;
+  hardware.enableRedistributableFirmware = true;
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+
+  # Firmware específico para b43 (ucode15.fw para BCM4312 LP-PHY)
+  hardware.firmware = [ pkgs.b43Firmware_5_1_138 ];
+
+  # Força carregamento dos módulos corretos
+  boot.initrd.kernelModules = [ "ssb" "b43" "btusb" ];
+  boot.kernelModules = [ "ssb" "b43" ];
+
+  # Evita conflitos com outros drivers Broadcom
+  boot.blacklistedKernelModules = [ "bcma" "brcmsmac" "wl" ];
+
+  ############################################
+  # Pacotes de firmware e utilitários (adicione ou mescle com system-packages.nix)
+  ############################################
+  environment.systemPackages = with pkgs; [
+    linux-firmware
+    bluez
+    blueman
+    b43-fwcutter          # Para extrair firmware manualmente se necessário
+    wirelesstools         # iwconfig, etc.
+    pciutils              # lspci
+    usbutils              # lsusb
+    rfkill                # rfkill list
+  ];
+
+  ############################################
+  # Bootloader (GRUB)
+  ############################################
+  boot.loader.grub.enable = true;
+  boot.loader.grub.devices = [ "/dev/sda" ]; # disco de boot principal
+  boot.loader.grub.useOSProber = false; # evita warnings de outros OS
 
   ############################################
   # System state version
