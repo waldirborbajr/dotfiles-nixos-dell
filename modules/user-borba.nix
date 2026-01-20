b289259a9199163ae15ebe98bccb089ba326a22b
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-19 21:15:12 -0300

    Update user-borba.nix

--- modules/user-borba.nix
+++ modules/user-borba.nix
@@ -7,16 +7,32 @@
   users.users.borba = {
     isNormalUser = true;
     description = "BORBA JR W";
+
+    # Shell padrão do usuário
     shell = pkgs.zsh;
 
     extraGroups = [
       "wheel"
       "networkmanager"
-      "docker"
-      "libvirtd"
-      "dialout"   # required for CHIRP / serial radios
+      "docker"       # ✅ Adicionado aqui diretamente
+      "libvirtd"    
+      "dialout"      # required for CHIRP / serial radios
     ];
   };
+
+  ############################################
+  # Auto-login
+  ############################################
+  services.xserver.displayManager.autoLogin = {
+    enable = true;
+    user = "borba";
+  };
+
+  ############################################
+  # Disable TTY competition
+  ############################################
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
 
   ############################################
   # Passwordless sudo
