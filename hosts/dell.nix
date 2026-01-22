566a1437ef19b656990b1bc2c2a753b0d8061c8e
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-22 13:38:55 -0300

    Update dell.nix

--- hosts/dell.nix
+++ hosts/dell.nix
@@ -1,46 +1,10 @@
 # hosts/dell.nix
 # ---
-#     nixos-config/hosts/dell.nix
+# nixos-config/hosts/dell.nix
 
-{ config, pkgs, lib, ... }:
+{ ... }:
 {
-
-  system.stateVersion = "25.11";
-
-  ############################################
-  # Hardware
-  ############################################
   imports = [
     ../profiles/dell.nix
-    ../modules/hardware/dell.nix
-    ../modules/performance/dell.nix
-    ../hardware-configuration-dell.nix
   ];
-  
-  ############################################
-  # Hardware
-  ############################################
-  #imports = [
-  #  ../modules/hardware/dell.nix
-  #  ../modules/performance/dell.nix
-  #  ../hardware-configuration-dell.nix
-  #];
-
-  ############################################
-  # Host identity
-  ############################################
-  networking.hostName = "dell-nixos";
-  ############################################
-  # Bootloader (Legacy BIOS)
-  ############################################
-  boot.loader.grub.enable = true;
-  boot.loader.grub.devices = [ "/dev/sda" ];
-  ############################################
-  # Keyboard (Dell)
-  ############################################
-  console.keyMap = "br-abnt2";
-  services.xserver.xkb = {
-    layout = "br";
-    variant = "abnt2";
-  };
 }
