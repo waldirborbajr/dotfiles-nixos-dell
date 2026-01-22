2ce46abe596ff6631894db37e8b37f6fdffd4842
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-21 22:34:26 -0300

    Update core.nix

--- core.nix
+++ core.nix
@@ -1,3 +1,4 @@
+# core.nix
 { ... }:
 
 {
@@ -6,42 +7,42 @@
     ##########################################
     # Core system
     ##########################################
-    ../modules/base.nix
-    ../modules/networking.nix
-    ../modules/audio.nix
-    ../modules/fonts.nix
-    ../modules/nixpkgs.nix
+    ./modules/base.nix
+    ./modules/networking.nix
+    ./modules/audio.nix
+    ./modules/fonts.nix
+    ./modules/nixpkgs.nix
 
     ##########################################
     # Desktop
     ##########################################
-    ../modules/desktops/gnome.nix
-    ../modules/autologin.nix
+    ./modules/desktops/gnome.nix
+    ./modules/autologin.nix
 
     ##########################################
     # Containers / Virtualization
     ##########################################
-    ../modules/containers/docker.nix
-    ../modules/containers/k3s.nix
-    ../modules/virtualization/libvirt.nix
+    ./modules/containers/docker.nix
+    ./modules/containers/k3s.nix
+    ./modules/virtualization/libvirt.nix
 
     ##########################################
     # Users / Dev
     ##########################################
-    ../modules/users/borba.nix
-    ../modules/system-packages.nix
-    ../modules/python/default.nix
-    ../modules/nodejs/default.nix
+    ./modules/users/borba.nix
+    ./modules/system-packages.nix
+    ./modules/python/default.nix
+    ./modules/nodejs/default.nix
 
     ##########################################
     # Flatpak
     ##########################################
-    ../modules/flatpak/enable.nix
-    ../modules/flatpak/packages.nix
+    ./modules/flatpak/enable.nix
+    ./modules/flatpak/packages.nix
 
     ##########################################
     # Access
     ##########################################
-    ../modules/ssh.nix
+    ./modules/ssh.nix
   ];
 }
