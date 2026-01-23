4a0db7bb246d6dfb7b86e9ed50fe4f2f55d58850
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-23 18:29:49 -0300

    Update devops.nix

--- modules/features/devops.nix
+++ modules/features/devops.nix
@@ -1,13 +1,49 @@
 # modules/features/devops.nix
-{ lib, devopsEnabled ? false, ... }:
+# ---
+{ config, lib, pkgs, devopsEnabled ? false, ... }:
 
+let
+  enable = devopsEnabled;
+in
 {
-  virtualisation.docker.enable = lib.mkDefault false;
-  services.k3s.enable = lib.mkDefault false;
+  config = lib.mkMerge [
+    # Default OFF (serviços não sobem)
+    {
+      virtualisation.docker.enable = lib.mkDefault false;
+      services.k3s.enable = lib.mkDefault false;
+    }
 
-  config = lib.mkIf devopsEnabled {
-    virtualisation.docker.enable = true;
-    virtualisation.docker.enableOnBoot = true;
-    services.k3s.enable = true;
-  };
+    # DEVOPS=1 -> ON
+    (lib.mkIf enable {
+      virtualisation.docker = {
+        enable = true;
+        enableOnBoot = true;
+        daemon.settings.features.buildkit = true;
+      };
+
+      services.k3s = {
+        enable = true;
+        role = "server";
+        extraFlags = [
+          "--write-kubeconfig-mode=644"
+          "--disable=traefik"
+          "--disable=servicelb"
+        ];
+      };
+
+      networking.firewall.allowedTCPPorts = [ 6443 ];
+
+      # Tooling (opcional aqui; pode remover se já estiver em system-packages)
+      environment.systemPackages = with pkgs; [
+        docker
+        docker-compose
+        docker-buildx
+        lazydocker
+        k9s
+        cri-tools
+      ];
+
+      users.users.borba.extraGroups = lib.mkAfter [ "docker" ];
+    })
+  ];
 }
