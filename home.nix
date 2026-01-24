c1b6e5febb98051fa511ef724a335b0aa4c1ceec
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-24 17:38:57 -0300

    Update home.nix

--- home.nix
+++ home.nix
@@ -5,4 +5,13 @@
   home.stateVersion = "25.11";
   home.username = "borba";
   home.homeDirectory = lib.mkForce "/home/borba";
+
+  # Importa os módulos que você quer ativar (padrão modular mantido)
+  imports = [
+    ./modules/apps/zsh.nix
+    # Futuro: ./modules/apps/fzf.nix (se criar)
+    # ./modules/apps/uv.nix
+    # ./modules/apps/poetry.nix
+    # etc.
+  ];
 }
