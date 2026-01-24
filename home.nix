d380d58adc6fd949b345251b661d6a8aebaaca73
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-24 17:53:11 -0300

    Update home.nix

--- home.nix
+++ home.nix
@@ -10,6 +10,7 @@
   imports = [
     ./modules/apps/zsh.nix
     ./modules/apps/fzf.nix
+    ./modules/apps/git.nix
     # Futuro: ./modules/apps/fzf.nix (se criar)
     # ./modules/apps/uv.nix
     # ./modules/apps/poetry.nix
