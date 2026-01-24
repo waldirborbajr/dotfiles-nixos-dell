a0249c35ce1810b9aa93e4bea3bd3c4ba0449d80
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-24 17:19:11 -0300

    Update home.nix

--- home.nix
+++ home.nix
@@ -1,25 +1,38 @@
 # home.nix
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   # Obrigatório: versão do state (use a do seu nixpkgs)
   home.stateVersion = "25.11";
 
   # Nome do usuário e home (deve bater com o seu)
-  home.username = "waldir";               # ← mude para o seu username real
-  home.homeDirectory = "/home/waldir";    # ou "/home/<seu-user>"
+  home.username = "waldir";  # ← confirme que é exatamente "waldir" (ou mude para o correto)
+  home.homeDirectory = lib.mkForce "/home/waldir";  # ← FORÇA o valor correto, ignora /var/empty
 
   # Teste simples: ativa fzf com integração zsh (é o que você quer!)
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
+    defaultOptions = [
+      "--height 60%"
+      "--layout=reverse"
+      "--border"
+      "--info=inline-right"
+      "--ansi"
+    ];
   };
 
-  # Pacotes só para o seu usuário (opcional, pode mover coisas daqui do systemPackages)
+  # Pacotes só para o seu usuário (adicione o que quiser mover do systemPackages)
   home.packages = with pkgs; [
-    # zoxide eza bat ripgrep fd tree  # ← exemplo, tire se já tem no system
+    zoxide
+    eza
+    bat
+    ripgrep
+    fd
+    tree
+    # ... outros que você usava globalmente
   ];
 
-  # Aqui no futuro você migra seu zsh completo
+  # Aqui no futuro migramos seu zsh completo
   # programs.zsh = { ... };
 }
