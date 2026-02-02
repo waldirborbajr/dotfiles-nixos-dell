7470c8bceadd51d8e6eb43917aafd764f674f661
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-02-02 01:17:58 +0000

    modified:   modules/apps/clipboard.nix

--- modules/apps/clipboard.nix
+++ modules/apps/clipboard.nix
@@ -5,10 +5,15 @@
 {
   config = lib.mkIf config.apps.clipboard.enable {
     home.packages = with pkgs; [
-      xclip
-      wl-clipboard
-      clipster
-      flameshot
+      xclip          # X11 clipboard
+      wl-clipboard   # Wayland clipboard
+      clipster       # Clipboard manager
+      
+      # Wayland screenshot tools (flameshot replacement)
+      grim           # Screenshot utility for Wayland
+      slurp          # Screen area selector for Wayland
+      swappy         # Screenshot editor (like flameshot)
+      grimblast      # Convenient wrapper for grim+slurp
     ];
   };
 }
